cf3af7e8f0b9398d9662af555f642b82759dfa6a
Eduardo Souza <dudd.webdeveloper@gmail.com>
2026-08-30 23:37:52 -0300

    Add Main class to demonstrate Carro functionality

---
+++ Logica_Programacao/POO | Associação Carro e Motor (Composição)/Main.java
@@ -0,0 +1,18 @@
+// Main
+
+
+public class Main {
+   static void main() {
+
+
+       Carro carro = new Carro(
+               "Toyota",
+               "Supra",
+               2500,
+               80
+       );
+
+       carro.mostrarCarro();
+
+   }
+}
