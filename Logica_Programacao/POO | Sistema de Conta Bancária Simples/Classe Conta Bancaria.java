cca3a763cf990d460b9d65ca63065c8902c61c02
Eduardo Souza <dudd.webdeveloper@gmail.com>
2026-08-31 00:19:56 -0300

    Create ContaBancaria class for bank account management

--- Logica_Programacao/POO | Sistema de Conta Bancária Simples/Classe Conta Bancaria.java
+++ Logica_Programacao/POO | Sistema de Conta Bancária Simples/Classe Conta Bancaria.java
@@ -1,4 +1,4 @@
-// Classe:
+// Classe Conta Bancaria
 
     import java.util.Scanner;
     public class ContaBancaria {
@@ -48,42 +48,3 @@
         }
 
     }
-
-// Main:
-
-import java.util.Scanner;
-public class Principal2 {
-    static void main() {
-
-        ContaBancaria contaBancaria = new ContaBancaria(
-          "marcelo",
-          1,
-          100
-        );
-        Scanner scanner = new Scanner(System.in);
-        int escolha = 0;
-        int recorrencia = 1;
-
-        System.out.println("Seja bem vindo ao Banco.");
-
-            do{
-                System.out.println("O que deseja fazer?");
-                System.out.println("1 - Realizar um Saque.");
-                System.out.println("2 - Realizar um Depósito.");
-                System.out.println("3 - Ver Saldo e Informações.");
-                System.out.println("4 - Sair.");
-                escolha = scanner.nextInt();
-
-                    switch (escolha){
-                        case 1: contaBancaria.sacar(); break;
-                        case 2: contaBancaria.depositar(); break;
-                        case 3: contaBancaria.mostrarSaldo(); break;
-                        case 4: recorrencia = 0; break;
-                    }
-
-            } while(recorrencia == 1);
-
-        System.out.println("Obrigado por usar nosso Banco.");
-        System.out.println("Até a próxima.");
-    }
-}
