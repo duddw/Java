96ccf60aa58052415700b5f14fc5703df9dfaeef
Eduardo Souza <dudd.webdeveloper@gmail.com>
2026-08-30 23:56:27 -0300

    Add Animal class with attributes and methods

---
+++ Logica_Programacao/POO | Exibição de Animais/Classe Animal.java
@@ -0,0 +1,47 @@
+// Classe Animal
+
+public class Animal {
+    private String nome;
+    private int idade;
+    private double peso;
+
+    public Animal(String nome, int idade, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void emitirSom(){
+
+    }
+
+    public void apresentarDados(){
+        System.out.println("Nome do Animal: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Peso: " + peso + "kg");
+    }
+}
