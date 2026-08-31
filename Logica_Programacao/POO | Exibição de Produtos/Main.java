1d36d2da1f73cbc7eaa8842eca57eb95094fe74b
Eduardo Souza <dudd.webdeveloper@gmail.com>
2026-08-31 00:03:55 -0300

    Add Main class to display product information

---
+++ Logica_Programacao/POO | Exibição de Produtos/Main.java
@@ -0,0 +1,13 @@
+// Main:
+
+public class Principal {
+    static void main() {
+
+        Produto p1 = new Produto (
+                "Mouse Gamer",
+                450
+        );
+
+        System.out.println(p1);
+    }
+}
