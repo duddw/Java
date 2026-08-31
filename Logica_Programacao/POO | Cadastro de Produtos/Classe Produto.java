39d91e1c1fca2113d231a99a8d0e098430e1c11c
Eduardo Souza <dudd.webdeveloper@gmail.com>
2026-08-30 23:46:47 -0300

    Update and rename Logica_Programacao/POO | Cadastro de Produtos.java to Logica_Programacao/POO | Cadastro de Produtos/Classe Produto.java

--- Logica_Programacao/POO | Cadastro de Produtos/Classe Produto.java
+++ Logica_Programacao/POO | Cadastro de Produtos/Classe Produto.java
@@ -1,4 +1,4 @@
-// Classe:
+// Classe Produto
 
 public class Produto {
 
@@ -18,27 +18,5 @@ public class Produto {
         System.out.println("Preço: R$" + preco);
         System.out.println("Quantidade: " + quantidade + " unidades");
         System.out.println();
-    }
-}
-
-// Main:
-
-public class Principal {
-
-    static void main() {
-
-        Produto produto1 = new Produto(
-                "batata",
-                6,
-                40
-        );
-        produto1.apresentarProduto();
-
-        Produto produto2 = new Produto(
-                "Tomate",
-                4,
-                40
-        );
-        produto2.apresentarProduto();
     }
 }
